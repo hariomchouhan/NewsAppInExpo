2654a0a8a4093de79a6c7f3bb3d46d92db092bef
Hariom <hariomchouhan430@gmail.com>
2025-01-26 14:15:17 +0530

    Set the Trending  news is pressable and move to the news details route

--- components/NewsList.tsx
+++ components/NewsList.tsx
@@ -1,8 +1,9 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { NewsDataType } from '@/types'
 import { Colors } from '@/constants/Colors'
 import Loading from './Loading'
+import { Link } from 'expo-router'
 
 type Props = {
     newsList: NewsDataType[]
@@ -16,17 +17,21 @@ const NewsList = ({ newsList }: Props) => {
                     <Loading size="large" />
                 ) : (
                     newsList?.map((item: any, index: number) => (
-                        <View key={index} style={styles.itemContainer}>
-                            <Image source={{ uri: item.image_url }} style={styles.itemImage} />
-                            <View style={styles.itemInfo}>
-                                <Text style={styles.itemCategory}>{item.category}</Text>
-                                <Text style={styles.itemTitle}>{item.title}</Text>
-                                <View style={styles.itemSourceInfo}>
-                                    <Image source={{ uri: item.source_icon }} style={styles.itemSourceImage} />
-                                    <Text style={styles.itemSourceName}>{item.source_name}</Text>
+                        <Link href={`/news/${item.article_id}`} asChild key={index}>
+                            <TouchableOpacity>
+                                <View style={styles.itemContainer}>
+                                    <Image source={{ uri: item.image_url }} style={styles.itemImage} />
+                                    <View style={styles.itemInfo}>
+                                        <Text style={styles.itemCategory}>{item.category}</Text>
+                                        <Text style={styles.itemTitle}>{item.title}</Text>
+                                        <View style={styles.itemSourceInfo}>
+                                            <Image source={{ uri: item.source_icon }} style={styles.itemSourceImage} />
+                                            <Text style={styles.itemSourceName}>{item.source_name}</Text>
+                                        </View>
+                                    </View>
                                 </View>
-                            </View>
-                        </View>
+                            </TouchableOpacity>
+                        </Link>
                     )
                     ))
             }
