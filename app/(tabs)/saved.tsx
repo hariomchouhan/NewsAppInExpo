39b3cf07d8a0b5c2d10878879893c6dbace199d0
Hariom <hariomchouhan430@gmail.com>
2025-01-27 11:52:55 +0530

    Complete to display all bookmark posts in saved screen

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,22 +1,78 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { Link, Stack } from "expo-router";
+import Loading from "@/components/Loading";
+import { NewsItem } from "@/components/NewsList";
+import { useIsFocused } from "@react-navigation/native";
 
-type Props = {}
+type Props = {};
 
 const Page = (props: Props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Saved Screen</Text>
-    </View>
-  )
-}
+  const [bookmarkNews, setBookmarkNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const isFocused = useIsFocused()
 
-export default Page
+  useEffect(() => {
+    fetchBookmark();
+  }, [isFocused]);
+  const fetchBookmark = async () => {
+    await AsyncStorage.getItem("bookmark").then(async (token) => {
+      const res = token ? JSON.parse(token) : null;
+      setIsLoading(true)
+      if (res) {
+        // console.log("Bookmark res: ", res);
+        let query_string = res.join(",");
+        // console.log(query_string);
+
+        const response = await axios.get(
+          `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&id=${query_string}`
+        );
+        setBookmarkNews(response.data.results);
+        setIsLoading(false);
+      } else {
+        setBookmarkNews([]);
+        setIsLoading(false);
+      }
+    });
+  };
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerShown: true,
+        }}
+      />
+      <View style={styles.container}>
+        {isLoading ? (
+          <Loading size={"large"} />
+        ) : (
+          <FlatList
+            data={bookmarkNews}
+            keyExtractor={(_, index) => `list_item${index}`}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item, index }) => {
+              return (
+                <Link href={`/news/${item.article_id}`} asChild key={index}>
+                  <TouchableOpacity>
+                    <NewsItem item={item} />
+                  </TouchableOpacity>
+                </Link>
+              );
+            }}
+          />
+        )}
+      </View>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    margin: 20,
   },
-})
+});
+
+export default Page;
