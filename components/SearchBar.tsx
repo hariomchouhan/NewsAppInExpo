d0471883b0297fd450dff8deda96885c47362c99
Hariom <hariomchouhan430@gmail.com>
2025-01-27 13:25:11 +0530

    fix the SearchBar for Android

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,29 +1,34 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
-import { Ionicons } from '@expo/vector-icons'
-import { Colors } from '@/constants/Colors'
+import { Platform, StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "@/constants/Colors";
 
 type Props = {
   withHorizontailPadding: boolean;
   setSearchQuery: Function;
-}
+};
 
 const SearchBar = ({ withHorizontailPadding, setSearchQuery }: Props) => {
   return (
-    <View style={[styles.container, withHorizontailPadding && { marginHorizontal: 20 }]}>
+    <View
+      style={[
+        styles.container,
+        withHorizontailPadding && { marginHorizontal: 20 },
+      ]}
+    >
       <View style={styles.searchBar}>
-        <Ionicons name='search-outline' size={20} color={Colors.lightGrey} />
+        <Ionicons name="search-outline" size={20} color={Colors.lightGrey} />
         <TextInput
-          placeholder='Search'
+          placeholder="Search"
           placeholderTextColor={Colors.lightGrey}
           style={styles.searchText}
-          autoCapitalize='none'
-          onChangeText={query => setSearchQuery(query)}
+          autoCapitalize="none"
+          onChangeText={(query) => setSearchQuery(query)}
         />
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -31,18 +36,19 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   searchBar: {
-    backgroundColor: '#E4E4E4',
+    backgroundColor: "#E4E4E4",
     paddingHorizontal: 10,
-    paddingVertical: 12,
+    paddingVertical: Platform.OS === "ios" ? 12 : 4,
     borderRadius: 10,
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 10,
+    alignItems: "center",
   },
   searchText: {
     fontSize: 14,
     flex: 1,
     color: Colors.darkGrey,
-  }
-})
+  },
+});
 
-export default SearchBar
+export default SearchBar;
